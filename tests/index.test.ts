7f6854f99c493dc296adcec0a0ca30e15394ade7
Camh-git <cameronhillier1@gmail.com>
2023-08-09 18:55:53 +0100

    Inital commit
    Inital commit with the copied over TS environment (the same one as the roman numerals example).
    Placeholder functions and tests added.

---
+++ tests/index.test.ts
@@ -0,0 +1,17 @@
+import { play } from "../src/bowling";
+import "jest";
+
+describe("Happy path tests", () => {
+  test("Test high and low scores", () => {
+    // expect().toBe();
+  });
+  test("Calculate a variety of more normal scores", () => {});
+});
+
+describe("Test edge cases", () => {
+  test("Dont allow negatives (in a variety of postions) ", () => {});
+
+  test("Dont allow values greater than 10", () => {});
+
+  test("Don't allow characters", () => {});
+});
