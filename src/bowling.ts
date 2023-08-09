f4769e54791a474d43013af5476e8cf468f90ab7
Camh-git <cameronhillier1@gmail.com>
2023-08-09 22:56:00 +0100

    Added play frame
    added playframe, ball rollers, some tests for them and an override interface to let us test with optional pre-determined value.

--- src/bowling.ts
+++ src/bowling.ts
@@ -1,1 +1,49 @@
-export function play() {}
+import { ballResult, parseBallResult } from "./types/ballResult";
+type frameScore = [ballResult, ballResult];
+
+export function playBall(remainingPins: number): number {
+  return Math.random() * (remainingPins + 1);
+}
+export interface playFrameTestingOverRide {
+  //Allows you to overwrite the random numbers so that playFrame can be tested
+  ballScores?: Array<number>;
+}
+export function playFrame(overRide: playFrameTestingOverRide): frameScore {
+  //setup our frame and run the first round
+  let frameResult: frameScore = [0, 0];
+  let pinsRemaining = 10 - playBall(10);
+  if (overRide.ballScores) {
+    //using the testing override
+    frameResult[0] = parseBallResult(overRide.ballScores[0]);
+    pinsRemaining = 10 - overRide.ballScores[0];
+  } else {
+    //running normaly
+    frameResult[0] = parseBallResult(10 - pinsRemaining);
+  }
+  console.log(`remaining pins: ${pinsRemaining}<<<<<<<<<<<<<<<<<<<<<<`);
+  //play a second ball if you didn't get a strike
+  if (pinsRemaining > 0) {
+    if (overRide.ballScores) {
+      //using the testing override
+      pinsRemaining = pinsRemaining - overRide.ballScores[1];
+    } else {
+      pinsRemaining = pinsRemaining - playBall(pinsRemaining);
+    }
+
+    //check for spare
+    if (pinsRemaining === 0) {
+      frameResult[1] = "spare";
+    } else {
+      frameResult[1] = parseBallResult(
+        parseInt(frameResult[0]) - pinsRemaining
+      );
+    }
+  } else {
+    //set ball 2 to 0 if you got a strike
+    frameResult[1] = 0;
+  }
+
+  return frameResult;
+}
+
+export function calculateScore(frames: Array<frameScore>) {}
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,17 +1,48 @@
-import { play } from "../src/bowling";
+import {
+  calculateScore,
+  playFrame,
+  playFrameTestingOverRide,
+} from "../src/bowling";
 import "jest";
 
-describe("Happy path tests", () => {
+describe("Test playing a frame", () => {
+  let override: playFrameTestingOverRide = { ballScores: [10, 0] };
   test("Test high and low scores", () => {
-    // expect().toBe();
+    expect(playFrame(override)).toStrictEqual(["strike", 0]);
+    override.ballScores = [2, 8];
+    expect(playFrame(override)).toStrictEqual([2, "spare"]);
+    override.ballScores = [0, 0];
+    expect(playFrame(override)).toStrictEqual([0, 0]);
   });
-  test("Calculate a variety of more normal scores", () => {});
+  test("Test a variety of more normal scores", () => {
+    override.ballScores = [1, 0];
+    expect(playFrame(override)).toStrictEqual([1, 0]);
+    override.ballScores = [0, 1];
+    expect(playFrame(override)).toStrictEqual([0, 1]);
+    override.ballScores = [4, 3];
+    expect(playFrame(override)).toStrictEqual([4, 3]);
+    override.ballScores = [2, 6];
+    expect(playFrame(override)).toStrictEqual([2, 6]);
+  });
 });
 
-describe("Test edge cases", () => {
-  test("Dont allow negatives (in a variety of postions) ", () => {});
+describe("Test play frame edge cases", () => {
+  let override: playFrameTestingOverRide = { ballScores: [0, 0] };
+  test("Dont allow negatives (in either postion) ", () => {
+    override.ballScores = [-1, 0];
+    expect(playFrame(override)).toStrictEqual([0, 0]);
+    override.ballScores = [0, -1];
+    expect(playFrame(override)).toStrictEqual([0, 0]);
+  });
 
-  test("Dont allow values greater than 10", () => {});
+  test("Dont allow values greater than 10", () => {
+    override.ballScores = [11, 0];
+    expect(playFrame(override)).toStrictEqual([0, 0]);
+    override.ballScores = [0, 11];
+    expect(playFrame(override)).toStrictEqual([0, 0]);
+  });
 
-  test("Don't allow characters", () => {});
+  test("Don't allow characters", () => {
+    //Taken care of by TS not allowing the override or playBall to accept strings
+  });
 });
