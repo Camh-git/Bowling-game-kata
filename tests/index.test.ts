0e1c3610c7c3d49670777f81bd56df5e5093c73c
Camh-git <cameronhillier1@gmail.com>
2023-08-14 20:54:29 +0100

    Added score calculation test
    added tests for the calculate score function, including results with and without scores and strikes and games ending in spares/strikes.

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,6 +1,8 @@
 import {
   calculateScore,
   playFrame,
+  frameScore,
+  autoPlay,
   playFrameTestingOverRide,
 } from "../src/bowling";
 import "jest";
@@ -51,4 +53,57 @@ describe("Test play frame edge cases", () => {
   });
 
   //Blocking character input is taken care of by TS not allowing the override or playBall to accept strings
+});
+
+describe("Test calculating the score", () => {
+  let testResults: Array<frameScore> = [
+    [1, 2],
+    [2, 7],
+    [3, 6],
+    [4, 2],
+    [5, 3],
+    [6, 2],
+    [7, 2],
+    [8, 1],
+    [9, 0],
+    [0, 5],
+  ];
+  test("Check a set of scores with no spares or strikes", () => {
+    expect(calculateScore(testResults)).toBe(75);
+    (testResults[0][1] = 7), (testResults[3][1] = 7);
+    expect(calculateScore(testResults)).toBe(85);
+    testResults[0][1] = 3;
+    expect(calculateScore(testResults)).toBe(81);
+    (testResults[0][1] = 2), (testResults[3][1] = 2);
+  });
+  test("Check spares and strikes", () => {
+    //first make sure that testResults is back to default
+    expect(calculateScore(testResults)).toBe(75);
+    testResults[0] = ["strike", 0];
+    expect(calculateScore(testResults)).toBe(91); //75 + the 7 we added to frame 1 + the 9 we scored in frame 2
+    testResults[0] = [5, "spare"];
+    expect(calculateScore(testResults)).toBe(84); //75 + the 7 we added to frame 1 + the 2 from frame 2 ball 1
+    testResults[0] = [1, 2];
+  });
+  test("Test games ending in spares and strikes", () => {
+    //first make sure that testResults is back to default
+    expect(calculateScore(testResults)).toBe(75);
+    testResults[9] = ["strike", 0];
+    expect(calculateScore(testResults)).toBe(90); //70 from the first 9 frames + 10 from final frame + 5 from our overwritten extra ball + 5 from strike bonus
+    //get a strike with the bonus ball
+    expect(calculateScore(testResults)).toBe(90); //70 from the first 9 frames + 20 from final frame(w/strike) (15 from our stike on second ball + 5 from 3rd ball)
+    //Get a strike with all 3 bonus balls
+    expect(calculateScore(testResults)).toBe(100); //70 from the first 9 frames + 30 for our 3 strikes on the last frame
+    testResults[9] = [5, "spare"];
+    expect(calculateScore(testResults)).toBe(80); //75 + 5 from bonus ball
+  });
+});
+
+describe("Test calculate score edge cases", () => {
+  //ensure that it only runs with exactly 10 frames
+  //return 0 if any of the frame has a total greater than 10, or a ball less than 0
+});
+
+describe("Test autoplay and running a full game", () => {
+  const result = autoPlay();
 });
